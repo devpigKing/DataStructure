e0fc0c722204bb9aaa1517ba921406abb38d4ef8
jwhydh <devpigking@gmail.com>
2023-09-13 18:02:49 +0900

    feat : 네 값의 최댓값을 구하는 max4 메서드와 클래스입니다.

---
+++ src/chap01/max4.java
@@ -0,0 +1,33 @@
+package chap01;
+
+
+public class max4 {
+
+    static int max4(int a, int b, int c, int d) {
+
+        int max = 0;
+
+        if (a > max) {
+            max = a;
+        }
+
+        if (b > max) {
+            max = b;
+        }
+
+        if (c > max) {
+            max = c;
+        }
+
+        if (d > max) {
+            max = d;
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("max3(3,5,3,1) = " + max4(3,5,3,1));
+    }
+
+}
